2034c887d1d4092b08d36ab49fdbc1dd0e41aa4c
muhammad <mohamed.mo.vb@gmail.com>
2024-08-17 09:19:31 +0300

    add tow videos to how

--- app/components/how.tsx
+++ app/components/how.tsx
@@ -6,20 +6,33 @@ export default function HowToUse() {
       <h1 className="text-center text-3xl mb-10">كيف استخدم تطبيق دكان</h1>
 
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-        <iframe
-          className="w-full aspect-2"
-          src="https://www.youtube.com/embed/r68cICgHrwM"
-          title="الحصري سوره النجم"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
-
-        <div className="bg-black w-full text-white text-lg font-bold flex justify-center items-center">
-          SOON
+        <div className="bg-black w-full text-white text-lg font-bold flex justify-center items-center flex-col">
+          <video className="w-full h-full" controls>
+            <source
+              src="https://www.dropbox.com/scl/fi/gs0wgf75ns3on7pin4ma2/.mp4?rlkey=3lhl1z7n8dm0dvh9luw0puyhs&st=8hlc1l76&raw=1"
+              type="video/mp4"
+            />
+          </video>
+          <div>الاشتراك بالتطبيق</div>
         </div>
-        <div className="bg-black w-full text-white text-lg font-bold flex justify-center items-center">
+        <div className="bg-black w-full text-white text-lg font-bold flex justify-center items-center flex-col">
+          <video width="100%" height="100%" controls={true}>
+            <source
+              src="https://www.dropbox.com/scl/fi/hd5itizzwlc16wkw9ehaz/.mp4?rlkey=fcfk8ylxmbg4pxm0qelru2mqs&st=hqfkwsj3&raw=1"
+              type="video/mp4"
+            />
+          </video>
+          <div>الاصناف</div>
+        </div>
+        <div className="bg-black w-full text-white text-lg font-bold flex justify-center items-center flex-col">
           SOON
+          {/* <video className="w-full h-full" controls> */}
+          {/*   <source */}
+          {/*     src="https://www.dropbox.com/scl/fi/gs0wgf75ns3on7pin4ma2/.mp4?rlkey=3lhl1z7n8dm0dvh9luw0puyhs&st=8hlc1l76&raw=1" */}
+          {/*     type="video/mp4" */}
+          {/*   /> */}
+          {/* </video> */}
+          {/* <div>الاشتراك بالتطبيق</div> */}
         </div>
       </div>
     </section>
